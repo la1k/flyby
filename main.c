f8455b7fac0ba9d77585d1d2de50e1bc224476ba
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-01-02 14:01:50 +0100

    Added comments to string_array.

--- main.c
+++ main.c
@@ -15,97 +15,13 @@
 #define FLYBY_OPT_DOWNLINK_VFO 205
 
 /**
- * Returns true if specified option's value is a char within the short options char array (and has thus a short-hand version of the long option)
+ * Print flyby program usage to stdout.
  *
- * \param short_options Array over short option chars
- * \param long_option Option struct to check
- * \returns True if option has a short option, false otherwise
- **/
-bool has_short_option(const char *short_options, struct option long_option)
-{
-	const char *ptr = strchr(short_options, long_option.val);
-	if (ptr == NULL) {
-		return false;
-	}
-	return true;
-}
-
-/**
- * Print flyby program usage to stdout. Option descriptions are set here.
- *
- * \param name Name of program, use argv[0]
+ * \param program_name Name of program, use argv[0]
  * \param long_options List of long options used in getopts_long
  * \param short_options List of short options used in getopts_long
  **/
-void show_help(const char *name, struct option long_options[], const char *short_options)
-{
-	//display initial description
-	printf("\nUsage:\n");
-	printf("%s [options]\n\n", name);
-	printf("Options:\n");
-
-	int index = 0;
-	while (true) {
-		if (long_options[index].name == 0) {
-			break;
-		}
-
-		//display short option
-		if (has_short_option(short_options, long_options[index])) {
-			printf(" -%c,", long_options[index].val);
-		} else {
-			printf("    ");
-		}
-		
-		//display long option
-		printf("--%s", long_options[index].name);
-
-		//display usage information
-		switch (long_options[index].val) {
-			case 'u':
-				printf("=FILE\t\tupdate TLE database with TLE file FILE");
-				break;
-			case 't':
-				printf("=FILE\t\t\tuse FILE as TLE database file. Overrides user and system TLE database files");
-				break;
-			case 'q':
-				printf("=FILE\t\t\tuse FILE as QTH config file. Overrides existing QTH config file");
-				break;
-			case 'a':
-				printf("=SERVER_HOST\t\tconnect to a rotctl server with hostname SERVER_HOST and enable antenna tracking");
-				break;
-			case FLYBY_OPT_ROTCTL_PORT:
-				printf("=SERVER_PORT\t\tspecify rotctl server port");
-				break;
-			case 'H':
-				printf("=HORIZON\t\t\tspecify horizon threshold for when %s will start tracking an orbit", name);
-				break;
-			case 'U':
-				printf("=SERVER_HOST\tconnect to specified rigctl server for uplink frequency steering");
-				break;
-			case FLYBY_OPT_UPLINK_PORT:
-				printf("=SERVER_PORT\tspecify rigctl uplink port");
-				break;
-			case FLYBY_OPT_UPLINK_VFO:
-				printf("=VFO_NAME\tspecify rigctl uplink VFO");
-				break;
-			case 'D':
-				printf("=SERVER_HOST\tconnect to specified rigctl server for downlink frequency steering");
-				break;
-			case FLYBY_OPT_DOWNLINK_PORT:
-				printf("=SERVER_PORT\tspecify rigctl downlink port");
-				break;
-			case FLYBY_OPT_DOWNLINK_VFO:
-				printf("=VFO_NAME\tspecify rigctl downlink VFO");
-				break;
-			case 'h':
-				printf("\t\t\t\tShow help");
-				break;
-		}
-		index++;
-		printf("\n");
-	}
-}
+void show_help(const char *program_name, struct option long_options[], const char *short_options);
 
 #define ROTCTL_DEFAULT_HOST "localhost"
 #define ROTCTL_DEFAULT_PORT "4533\0\0"
@@ -114,39 +30,48 @@ void show_help(const char *name, struct option long_options[], const char *short
 #define RIGCTL_DOWNLINK_DEFAULT_HOST "localhost"
 #define RIGCTL_DOWNLINK_DEFAULT_PORT "4532\0\0"
 
+/**
+ * Dynamic size string array for the situations where we don't know in advance the number of filenames.
+ * Used for accumulating TLE filenames. Exchange by std::vector if we start using C++ instead. :^)
+ **/
 typedef struct {
-	int num_strings;
-	char **strings;
+	int available_size; //available size within string array
+	int num_strings; //current number of strings
+	char **strings; //strings
 } string_array_t;
 
-void string_array_add(string_array_t *string_array, const char *string)
-{
-	char **temp = realloc(string_array->strings, string_array->num_strings + 1);
-	if (temp != NULL) {
-		string_array->strings = temp;
-		string_array->strings[string_array->num_strings] = (char*)malloc(sizeof(char)*MAX_NUM_CHARS);
-		strncpy(string_array->strings[string_array->num_strings], string, MAX_NUM_CHARS);
-		string_array->num_strings++;
-	}
-}
+/**
+ * Add string to string array. Reallocates available space to twice the size when current available size is exceeded.
+ *
+ * \param string_array String array
+ * \param string String to add
+ * \return 0 on success, -1 on failure
+ **/
+int string_array_add(string_array_t *string_array, const char *string);
 
-char* string_array_get(string_array_t *string_array, int index)
-{
-	if (index < string_array->num_strings) {
-		return string_array->strings[index];
-	}
-	return NULL;
-}
+/**
+ * Get string at specified index from string array.
+ *
+ * \param string_array String array
+ * \param index Index at which we want to extract a string
+ * \return String at specified index
+ **/
+const char* string_array_get(string_array_t *string_array, int index);
 
-void string_array_free(string_array_t *string_array)
-{
-	for (int i=0; i < string_array->num_strings; i++) {
-		free(string_array->strings[i]);
-	}
-	free(string_array->strings);
-	string_array->num_strings = 0;
-	string_array->strings = NULL;
-}
+/**
+ * Get string array size.
+ *
+ * \param string_array String array
+ * \return Size
+ **/
+int string_array_size(string_array_t *string_array);
+
+/**
+ * Free memory allocated in string array.
+ *
+ * \param string_array String array to free
+ **/
+void string_array_free(string_array_t *string_array);
 
 int main (int argc, char **argv)
 {
@@ -199,7 +124,6 @@ int main (int argc, char **argv)
 		{0, 0, 0, 0}
 	};
 	char short_options[] = "u:t:q:a:H:U:D:h";
-	char **temp;
 	while (1) {
 		int option_index = 0;
 		int c = getopt_long(argc, argv, short_options, long_options, &option_index);
@@ -256,4 +180,141 @@ int main (int argc, char **argv)
 	for (int i=0; i < tle_cmd_filenames.num_strings; i++) {
 		printf("%s\n", tle_cmd_filenames.strings[i]);
 	}
+}
+
+/**
+ * Returns true if specified option's value is a char within the short options char array (and has thus a short-hand version of the long option)
+ *
+ * \param short_options Array over short option chars
+ * \param long_option Option struct to check
+ * \returns True if option has a short option, false otherwise
+ **/
+bool has_short_option(const char *short_options, struct option long_option)
+{
+	const char *ptr = strchr(short_options, long_option.val);
+	if (ptr == NULL) {
+		return false;
+	}
+	return true;
+}
+
+void show_help(const char *name, struct option long_options[], const char *short_options)
+{
+	//display initial description
+	printf("\nUsage:\n");
+	printf("%s [options]\n\n", name);
+	printf("Options:\n");
+
+	int index = 0;
+	while (true) {
+		if (long_options[index].name == 0) {
+			break;
+		}
+
+		//display short option
+		if (has_short_option(short_options, long_options[index])) {
+			printf(" -%c,", long_options[index].val);
+		} else {
+			printf("    ");
+		}
+		
+		//display long option
+		printf("--%s", long_options[index].name);
+
+		//display usage information
+		switch (long_options[index].val) {
+			case 'u':
+				printf("=FILE\t\tupdate TLE database with TLE file FILE. Multiple files can be specified using the same option multiple times (e.g. -u file1 -u file2 ...)");
+				break;
+			case 't':
+				printf("=FILE\t\t\tuse FILE as TLE database file. Overrides user and system TLE database files. Multiple files can be specified using the same option multiple times (e.g. -t file1 -t file2 ...)");
+				break;
+			case 'q':
+				printf("=FILE\t\t\tuse FILE as QTH config file. Overrides existing QTH config file");
+				break;
+			case 'a':
+				printf("=SERVER_HOST\t\tconnect to a rotctl server with hostname SERVER_HOST and enable antenna tracking");
+				break;
+			case FLYBY_OPT_ROTCTL_PORT:
+				printf("=SERVER_PORT\t\tspecify rotctl server port");
+				break;
+			case 'H':
+				printf("=HORIZON\t\t\tspecify horizon threshold for when %s will start tracking an orbit", name);
+				break;
+			case 'U':
+				printf("=SERVER_HOST\tconnect to specified rigctl server for uplink frequency steering");
+				break;
+			case FLYBY_OPT_UPLINK_PORT:
+				printf("=SERVER_PORT\tspecify rigctl uplink port");
+				break;
+			case FLYBY_OPT_UPLINK_VFO:
+				printf("=VFO_NAME\tspecify rigctl uplink VFO");
+				break;
+			case 'D':
+				printf("=SERVER_HOST\tconnect to specified rigctl server for downlink frequency steering");
+				break;
+			case FLYBY_OPT_DOWNLINK_PORT:
+				printf("=SERVER_PORT\tspecify rigctl downlink port");
+				break;
+			case FLYBY_OPT_DOWNLINK_VFO:
+				printf("=VFO_NAME\tspecify rigctl downlink VFO");
+				break;
+			case 'h':
+				printf("\t\t\t\tShow help");
+				break;
+		}
+		index++;
+		printf("\n");
+	}
+}
+
+int string_array_add(string_array_t *string_array, const char *string)
+{
+	//initialize
+	if (string_array->available_size < 1) {
+		string_array->strings = (char**)malloc(sizeof(char*));
+		string_array->available_size = 1;
+		string_array->num_strings = 0;
+	}
+
+	//extend size to twice the current size
+	if (string_array->num_strings+1 > string_array->available_size) {
+		printf("%d\n", string_array->available_size);
+		char **temp = realloc(string_array->strings, sizeof(char*)*string_array->available_size*2);
+		if (temp == NULL) {
+			return -1;
+		}
+		string_array->available_size = string_array->available_size*2;
+		string_array->strings = temp;
+	}
+
+	//copy string
+	string_array->strings[string_array->num_strings] = (char*)malloc(sizeof(char)*MAX_NUM_CHARS);
+	strncpy(string_array->strings[string_array->num_strings], string, MAX_NUM_CHARS);
+	string_array->num_strings++;
+	return 0;
+}
+
+const char* string_array_get(string_array_t *string_array, int index)
+{
+	if (index < string_array->num_strings) {
+		return string_array->strings[index];
+	}
+	return NULL;
+}
+
+void string_array_free(string_array_t *string_array)
+{
+	for (int i=0; i < string_array->num_strings; i++) {
+		free(string_array->strings[i]);
+	}
+	free(string_array->strings);
+	string_array->num_strings = 0;
+	string_array->available_size = 0;
+	string_array->strings = NULL;
+}
+
+int string_array_size(string_array_t *string_array)
+{
+	return string_array->num_strings;
 }
