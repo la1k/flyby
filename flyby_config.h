4dfbaa6647f77527114cf266c5ff82ffbb227b2d
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-01-23 15:07:41 +0100

    Reorganized flyby_config.h.

--- flyby_config.h
+++ flyby_config.h
@@ -16,19 +16,6 @@
 void tle_db_from_search_paths(struct tle_db *ret_tle_db);
 
 /**
- * Read transponder database from folders defined using the XDG file specification.
- * Database file is assumed to be located in {XDG_DATA_DIRS}/flyby/flyby.db and XDG_DATA_HOME/flyby/flyby.db.
- * A union over the files is used.
- *
- * Transponder entries defined in XDG_DATA_HOME take precedence over XDG_DATA_DIRS. XDG_DATA_DIRS
- * ordering decides precedence of entries defined across XDG_DATA_DIRS directories.
- *
- * \param tle_db Full TLE database for which transponder database entries are matched
- * \param transponder_db Returned transponder database
- **/
-void transponder_db_from_search_paths(const struct tle_db *tle_db, struct transponder_db *transponder_db);
-
-/**
  * Read TLE database from file.
  *
  * \param tle_file TLE database file
@@ -36,55 +23,6 @@ void transponder_db_from_search_paths(const struct tle_db *tle_db, struct transp
  * \return 0 on success, -1 otherwise
  **/
 int tle_db_from_file(const char *tle_file, struct tle_db *ret_db);
-
-/**
- * Read QTH information from file.
- *
- * \param qth_file .qth file
- * \param ret_observer Returned observer structure
- * \return 0 on success, -1 otherwise
- **/
-int qth_from_file(const char *qth_file, predict_observer_t *ret_observer);
-
-/**
- * Write QTH information to specified file.
- *
- * \param qth_path File path
- * \param qth Qth information to write
- **/
-void qth_to_file(const char *qth_path, predict_observer_t *qth);
-
-/**
- * Get local user qth filepath (~/.config/flyby/flyby.qth).
- **/
-char* qth_default_writepath();
-
-/**
- * Read transponder database from file. Only fields matching the TLE database fields are modified.
- *
- * \param db_file .db file
- * \param tle_db Previously read TLE database, for which fields from transponder database are matched
- * \param ret_db Returned transponder database
- * \return 0 on success, -1 otherwise
- **/
-int transponder_db_from_file(const char *db_file, const struct tle_db *tle_db, struct transponder_db *ret_db);
-
-/**
- * Used for determining from where the QTH file was read.
- **/
-enum qth_file_state {
-	QTH_FILE_HOME, //read from XDG_CONFIG_HOME
-	QTH_FILE_SYSTEMWIDE, //read from XDG_CONFIG_DIRS
-	QTH_FILE_NOTFOUND //not found
-};
-
-/**
- * Read flyby from XDG filepaths. Try XDG_CONFIG_HOME/flyby/flyby.qth first, then the paths in XDG_CONFIG_DIRS/flyby/flyby.qth.
- *
- * \param ret_observer Returned QTH information
- * \return Where the QTH file was read from, user home, system dir or not found at all
- **/
-enum qth_file_state qth_from_search_paths(predict_observer_t *ret_observer);
 
 /**
  * Update internal TLE database with newer TLE entries located within supplied file, and update the corresponding file databases.
@@ -120,5 +58,67 @@ enum tle_merge_behavior {
  * \param merge_opt Merge options
  **/
 void tle_db_merge(struct tle_db *new_db, struct tle_db *main_db, enum tle_merge_behavior merge_opt);
+
+/**
+ * Used for determining from where the QTH file was read.
+ **/
+enum qth_file_state {
+	QTH_FILE_HOME, //read from XDG_CONFIG_HOME
+	QTH_FILE_SYSTEMWIDE, //read from XDG_CONFIG_DIRS
+	QTH_FILE_NOTFOUND //not found
+};
+
+/**
+ * Read flyby from XDG filepaths. Try XDG_CONFIG_HOME/flyby/flyby.qth first, then the paths in XDG_CONFIG_DIRS/flyby/flyby.qth.
+ *
+ * \param ret_observer Returned QTH information
+ * \return Where the QTH file was read from, user home, system dir or not found at all
+ **/
+enum qth_file_state qth_from_search_paths(predict_observer_t *ret_observer);
+
+/**
+ * Read QTH information from file.
+ *
+ * \param qth_file .qth file
+ * \param ret_observer Returned observer structure
+ * \return 0 on success, -1 otherwise
+ **/
+int qth_from_file(const char *qth_file, predict_observer_t *ret_observer);
+
+/**
+ * Write QTH information to specified file.
+ *
+ * \param qth_path File path
+ * \param qth Qth information to write
+ **/
+void qth_to_file(const char *qth_path, predict_observer_t *qth);
+
+/**
+ * Get local user qth filepath (~/.config/flyby/flyby.qth).
+ **/
+char* qth_default_writepath();
+
+/**
+ * Read transponder database from folders defined using the XDG file specification.
+ * Database file is assumed to be located in {XDG_DATA_DIRS}/flyby/flyby.db and XDG_DATA_HOME/flyby/flyby.db.
+ * A union over the files is used.
+ *
+ * Transponder entries defined in XDG_DATA_HOME take precedence over XDG_DATA_DIRS. XDG_DATA_DIRS
+ * ordering decides precedence of entries defined across XDG_DATA_DIRS directories.
+ *
+ * \param tle_db Full TLE database for which transponder database entries are matched
+ * \param transponder_db Returned transponder database
+ **/
+void transponder_db_from_search_paths(const struct tle_db *tle_db, struct transponder_db *transponder_db);
+
+/**
+ * Read transponder database from file. Only fields matching the TLE database fields are modified.
+ *
+ * \param db_file .db file
+ * \param tle_db Previously read TLE database, for which fields from transponder database are matched
+ * \param ret_db Returned transponder database
+ * \return 0 on success, -1 otherwise
+ **/
+int transponder_db_from_file(const char *db_file, const struct tle_db *tle_db, struct transponder_db *ret_db);
 
 #endif
