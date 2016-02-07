162e62b1a2538471900f01e632df8aa7b073ce4b
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-02-07 16:26:13 +0100

    Rearranged menu interface for whitelisting into a more abstract interface, in order to facilitate more advanced behavior later.

--- flyby_whitelist.c
+++ flyby_whitelist.c
@@ -17,45 +17,6 @@ void pattern_prepare(char *string)
 	for (int i=0; i < length; i++) {
 		string[i] = toupper(string[i]);
 	}
-
-}
-
-bool pattern_match(const char *string, const char *pattern) {
-	return (strlen(pattern) == 0) || (strstr(string, pattern) != NULL);
-}
-
-ITEM** prepare_menu_items(const struct tle_db *tle_db, const char *pattern, int *tle_index)
-{
-	int max_num_choices = tle_db->num_tles;
-	ITEM **my_items = (ITEM **)calloc(max_num_choices + 1, sizeof(ITEM *));
-	int item_ind = 0;
-
-	//get all TLE names corresponding to the input pattern
-	for (int i = 0; i < max_num_choices; ++i) {
-		if (pattern_match(tle_db->tles[i].name, pattern)) {
-			my_items[item_ind] = new_item(tle_db->tles[i].name, "");
-			tle_index[item_ind] = i;
-			item_ind++;
-		}
-	}
-	my_items[item_ind] = NULL; //terminate the menu list
-	return my_items;
-}
-
-void mark_checked_tles(struct tle_db *tle_db, int *tle_index, ITEM** items)
-{
-	for (int i=0; i < tle_db->num_tles; i++) {
-		if (items[i] == NULL) {
-			break;
-		}
-
-		int index = tle_index[i];
-		if (tle_db_entry_enabled(tle_db, index)) {
-			set_item_value(items[i], TRUE);
-		} else {
-			set_item_value(items[i], FALSE);
-		}
-	}
 }
 
 void free_menu_items(ITEM ***items)
@@ -75,30 +36,213 @@ void free_menu_items(ITEM ***items)
 	free(*items);
 }
 
-void toggle_all(struct tle_db* tle_db, ITEM** items, int *tle_index)
-{
-	//check if all items in menu are enabled
-	bool all_enabled = true;
-	for (int i=0; i < tle_db->num_tles; i++) {
-		if (items[i] == NULL) {
+bool pattern_match(const char *string, const char *pattern) {
+	return (strlen(pattern) == 0) || (strstr(string, pattern) != NULL);
+}
+
+struct selectable_list_entry {
+	char *displayed_name;
+	bool enabled;
+
+	int num_descriptors;
+	char **descriptors;
+};
+
+struct pattern_match {
+	int num_matches;
+	char **matches;
+};
+
+struct selectable_list {
+	int max_num_items;
+	ITEM** displayed_entries;
+	int num_displayed_entries;
+	MENU* menu;
+	struct selectable_list_entry *entries;
+	int *entry_mapping;
+};
+
+void selectable_list_entry_free(struct selectable_list_entry *list_entry) {
+	free(list_entry->displayed_name);
+	for (int i=0; i < list_entry->num_descriptors; i++) {
+		free(list_entry->descriptors[i]);
+	}
+	free(list_entry->descriptors);
+}
+
+void selectable_list_free(struct selectable_list *list) {
+	if (list->num_displayed_entries > 0) {
+		unpost_menu(list->menu);
+	}
+	free_menu(list->menu);
+	free_menu_items(&(list->displayed_entries));
+
+	for (int i=0; i < list->max_num_items; i++) {
+		selectable_list_entry_free(&(list->entries[i]));
+	}
+	free(list->entries);
+	free(list->entry_mapping);
+}
+
+void selectable_list_pattern_match(struct selectable_list *list, const char *pattern);
+
+void selectable_list_from_tle_db(struct selectable_list *list, const struct tle_db *db, WINDOW *my_menu_win) {
+	list->num_displayed_entries = 0;
+	list->max_num_items = db->num_tles;
+	list->displayed_entries = (ITEM **)calloc(list->max_num_items + 1, sizeof(ITEM*));
+	list->entry_mapping = (int*)calloc(list->max_num_items, sizeof(int));
+	list->entries = (struct selectable_list_entry*)malloc(sizeof(struct selectable_list_entry)*list->max_num_items);
+	for (int i=0; i < db->num_tles; i++) {
+		list->entries[i].displayed_name = strdup(db->tles[i].name);
+		list->entries[i].num_descriptors = 2;
+		list->entries[i].descriptors = (char**)malloc(sizeof(char*)*list->entries[i].num_descriptors);
+		list->entries[i].descriptors[0] = strdup(db->tles[i].name);
+		list->entries[i].descriptors[1] = strdup(db->tles[i].filename);
+		list->entries[i].enabled = tle_db_entry_enabled(db, i);
+
+		list->displayed_entries[i] = new_item(list->entries[i].displayed_name, "");
+	}
+	list->displayed_entries[db->num_tles] = NULL;
+	list->num_displayed_entries = db->num_tles;
+
+	MENU *my_menu = new_menu(list->displayed_entries);
+	list->menu = my_menu;
+
+	set_menu_back(my_menu,COLOR_PAIR(1));
+	set_menu_fore(my_menu,COLOR_PAIR(5)|A_BOLD);
+
+	set_menu_win(my_menu, my_menu_win);
+
+	int max_width, max_height;
+	getmaxyx(my_menu_win, max_height, max_width);
+	set_menu_sub(my_menu, derwin(my_menu_win, max_height - 3, max_width - 2, 2, 1));
+	set_menu_format(my_menu, LINES-14, 1);
+
+	set_menu_mark(my_menu, " * ");
+	menu_opts_off(my_menu, O_ONEVALUE);
+	post_menu(my_menu);
+
+	selectable_list_pattern_match(list, "");
+}
+
+void selectable_list_pattern_match(struct selectable_list *list, const char *pattern) {
+	/*
+	if (valid_menu) {
+		strncpy(curr_item, item_name(current_item(my_menu)), MAX_NUM_CHARS);
+	}*/
+
+	if (list->num_displayed_entries > 0) {
+		unpost_menu(list->menu);
+		list->num_displayed_entries = 0;
+	}
+
+	ITEM **temp_items = (ITEM **)calloc(list->max_num_items + 1, sizeof(ITEM *));
+	int item_ind = 0;
+
+	//get all TLE names corresponding to the input pattern
+	for (int i = 0; i < list->max_num_items; ++i) {
+		if (pattern_match(list->entries[i].descriptors[0], pattern)) {
+			temp_items[item_ind] = new_item(list->entries[i].displayed_name, "");
+			list->entry_mapping[item_ind] = i;
+			item_ind++;
+		}
+	}
+	temp_items[item_ind] = NULL; //terminate the menu list
+
+	if (temp_items[0] != NULL) {
+		set_menu_items(list->menu, temp_items);
+		list->num_displayed_entries = item_ind;
+		free_menu_items(&(list->displayed_entries));
+		list->displayed_entries = temp_items;
+		post_menu(list->menu);
+	} else {
+		free(temp_items);
+	}
+
+	/* remove old items
+	if (retval == E_OK) {
+	free_menu_items(&items);
+	items = temp_items;
+	set_menu_pattern(my_menu, curr_item);
+	mark_checked_tles(tle_db, tle_index, items);
+	} else {
+	free_menu_items(&temp_items);
+	}
+	return my_items;*/
+
+	for (int i=0; i < list->max_num_items; i++) {
+		if (list->displayed_entries[i] == NULL) {
 			break;
 		}
-		if (item_value(items[i]) == FALSE) {
+
+		int index = list->entry_mapping[i];
+		if (list->entries[index].enabled) {
+			set_item_value(list->displayed_entries[i], TRUE);
+		} else {
+			set_item_value(list->displayed_entries[i], FALSE);
+		}
+	}
+}
+
+void selectable_list_to_tle_db(struct selectable_list *list, struct tle_db *db) {
+	for (int i=0; i < list->max_num_items; i++) {
+		tle_db_entry_set_enabled(db, i, list->entries[i].enabled);
+	}
+}
+
+void selectable_list_toggle(struct selectable_list *list);
+
+bool selectable_list_handle(struct selectable_list *list, int c) {
+	if (list->num_displayed_entries <= 0) {
+		return false;
+	}
+
+	switch(c) {
+		case KEY_DOWN:
+		 	menu_driver(list->menu, REQ_DOWN_ITEM);
+			break;
+		case KEY_UP:
+			menu_driver(list->menu, REQ_UP_ITEM);
+			break;
+		case KEY_NPAGE:
+			menu_driver(list->menu, REQ_SCR_DPAGE);
+			break;
+		case KEY_PPAGE:
+			menu_driver(list->menu, REQ_SCR_UPAGE);
+			break;
+		case 'a':
+			selectable_list_toggle(list);
+			break;
+		case ' ':
+			pos_menu_cursor(list->menu);
+			menu_driver(list->menu, REQ_TOGGLE_ITEM);
+
+			int index = list->entry_mapping[item_index(current_item(list->menu))];
+			list->entries[index].enabled = !(list->entries[index].enabled);
+			break;
+		default:
+			return false;
+			break;
+	}
+	return true;
+}
+
+void selectable_list_toggle(struct selectable_list *list) {
+	//check if all items in menu are enabled
+	bool all_enabled = true;
+	for (int i=0; i < list->num_displayed_entries; i++) {
+		if (item_value(list->displayed_entries[i]) == FALSE) {
 			all_enabled = false;
 		}
 	}
 
-	for (int i=0; i < tle_db->num_tles; i++) {
-		if (items[i] == NULL) {
-			break;
-		}
-
+	for (int i=0; i < list->num_displayed_entries; i++) {
 		if (all_enabled) {
-			set_item_value(items[i], FALSE);
-			tle_db_entry_set_enabled(tle_db, tle_index[i], false);
+			set_item_value(list->displayed_entries[i], FALSE);
+			list->entries[list->entry_mapping[i]].enabled = false;
 		} else {
-			set_item_value(items[i], TRUE);
-			tle_db_entry_set_enabled(tle_db, tle_index[i], true);
+			set_item_value(list->displayed_entries[i], TRUE);
+			list->entries[list->entry_mapping[i]].enabled = true;
 		}
 	}
 }
@@ -114,9 +258,8 @@ void EditWhitelist(struct tle_db *tle_db)
 	mvprintw(row++,0,"  flyby TLE whitelister                                                         ");
 	mvprintw(row++,0,"                                                                                ");
 
-	int retval;
 	int c;
-	MENU *my_menu;
+
 	WINDOW *my_menu_win;
 
 	int *tle_index = (int*)calloc(tle_db->num_tles, sizeof(int));
@@ -179,70 +322,27 @@ void EditWhitelist(struct tle_db *tle_db)
 
 	refresh();
 
-	/* Create items */
-	if (tle_db->num_tles > 0) {
-		ITEM **items = prepare_menu_items(tle_db, "", tle_index);
-		ITEM **temp_items = NULL;
-		char field_contents[MAX_NUM_CHARS] = {0};
-		char curr_item[MAX_NUM_CHARS] = {0};
+	struct selectable_list menu = {0};
+	selectable_list_from_tle_db(&menu, tle_db, my_menu_win);
 
-		bool valid_menu = (items[0] != NULL);
+	char field_contents[MAX_NUM_CHARS] = {0};
 
-		/* Create menu */
-		my_menu = new_menu(items);
+	refresh();
+	wrefresh(my_menu_win);
+	form_driver(form, REQ_VALIDATION);
+	wrefresh(form_win);
+	bool run_menu = true;
 
-		set_menu_back(my_menu,COLOR_PAIR(1));
-		set_menu_fore(my_menu,COLOR_PAIR(5)|A_BOLD);
+	while (run_menu) {
+		c = wgetch(my_menu_win);
 
-		/* Set main window and sub window */
-		set_menu_win(my_menu, my_menu_win);
-		set_menu_sub(my_menu, derwin(my_menu_win, LINES-(window_ypos + 5), window_width - 2, 2, 1));
-		set_menu_format(my_menu, LINES-14, 1);
-
-		/* Set menu mark to the string " * " */
-		set_menu_mark(my_menu, " * ");
-
-		menu_opts_off(my_menu, O_ONEVALUE);
-
-		/* Post the menu */
-		post_menu(my_menu);
-
-		mark_checked_tles(tle_db, tle_index, items);
-
-		refresh();
+		bool handled = selectable_list_handle(&menu, c);
 		wrefresh(my_menu_win);
-		form_driver(form, REQ_VALIDATION);
-		wrefresh(form_win);
-		bool run_menu = true;
 
-		while (run_menu) {
-			c = wgetch(my_menu_win);
-			switch(c) {
+		if (!handled) {
+			switch (c) {
 				case 'q':
 					run_menu = false;
-					break;
-				case KEY_DOWN:
-					if (valid_menu) menu_driver(my_menu, REQ_DOWN_ITEM);
-					break;
-				case KEY_UP:
-					if (valid_menu) menu_driver(my_menu, REQ_UP_ITEM);
-					break;
-				case KEY_NPAGE:
-					if (valid_menu) menu_driver(my_menu, REQ_SCR_DPAGE);
-					break;
-				case KEY_PPAGE:
-					if (valid_menu) menu_driver(my_menu, REQ_SCR_UPAGE);
-					break;
-				case 'a':
-					if (valid_menu) toggle_all(tle_db, items, tle_index);
-					break;
-				case ' ':
-					if (valid_menu) {
-						pos_menu_cursor(my_menu);
-						menu_driver(my_menu, REQ_TOGGLE_ITEM);
-						int index = tle_index[item_index(current_item(my_menu))];
-						tle_db_entry_set_enabled(tle_db, index, !tle_db_entry_enabled(tle_db, index));
-					}
 					break;
 				case KEY_BACKSPACE:
 					form_driver(form, REQ_DEL_PREV);
@@ -259,59 +359,19 @@ void EditWhitelist(struct tle_db *tle_db)
 					strncpy(field_contents, field_buffer(field[0], 0), MAX_NUM_CHARS);
 					pattern_prepare(field_contents);
 
-					if (valid_menu) {
-						strncpy(curr_item, item_name(current_item(my_menu)), MAX_NUM_CHARS);
-					}
-
-					/* Update menu with new items */
-					temp_items = prepare_menu_items(tle_db, field_contents, tle_index);
-
-					if (valid_menu) {
-						unpost_menu(my_menu);
-					}
-
-					if (temp_items[0] != NULL) {
-						retval = set_menu_items(my_menu, temp_items);
-					} else {
-						retval = E_BAD_ARGUMENT;
-					}
-
-					if (retval != E_OK) {
-						valid_menu = false;
-					} else {
-						valid_menu = true;
-						post_menu(my_menu);
-					}
-
-					/* remove old items */
-					if (retval == E_OK) {
-						free_menu_items(&items);
-						items = temp_items;
-						set_menu_pattern(my_menu, curr_item);
-						mark_checked_tles(tle_db, tle_index, items);
-					} else {
-						free_menu_items(&temp_items);
-					}
+					selectable_list_pattern_match(&menu, field_contents);
 
 					wrefresh(form_win);
 					break;
 			}
-			wrefresh(my_menu_win);
 		}
-
-		/* Unpost and free all the memory taken up */
-		unpost_menu(my_menu);
-		free_menu(my_menu);
-
-		free_menu_items(&items);
-
-		//update file
-		whitelist_write_to_default(tle_db);
-	} else {
-		refresh();
-		wrefresh(my_menu_win);
-		c = wgetch(my_menu_win);
 	}
+
+	selectable_list_to_tle_db(&menu, tle_db);
+	selectable_list_free(&menu);
+
+	//update file
+	whitelist_write_to_default(tle_db);
 
 	free_field(field[0]);
 	free(tle_index);
