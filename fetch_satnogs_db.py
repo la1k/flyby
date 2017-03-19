2c7cf97eb8319deadfac50773ebdc85e3b25b8c4
Øyvind Karlsen <oyvkar1@gmail.com>
2017-03-19 13:31:44 +0100

    Renamed for loop variable i to transponder

--- fetch_satnogs_db.py
+++ fetch_satnogs_db.py
@@ -27,43 +27,43 @@ with open(expanduser("~")+"/.local/share/flyby/flyby.db","w") as db:
     previous_norad_id = False
 
     # Iterate through SatNOGS database
-    for i in sorteddata:
+    for transponder in sorteddata:
         
         # Fill null fields with 0
         for key in ['uplink_low','uplink_high','downlink_low','downlink_high', 'baud']:
-            if i[key] is None:
-                i[key] = 0
+            if transponder[key] is None:
+                transponder[key] = 0
 
         # Single frequency transponders should have same high and low frequency
-        if i['uplink_high'] == 0:
-            i['uplink_high'] = i['uplink_low']
-        if i['downlink_high'] == 0:
-            i['downlink_high'] = i['downlink_low']
+        if transponder['uplink_high'] == 0:
+            transponder['uplink_high'] = transponder['uplink_low']
+        if transponder['downlink_high'] == 0:
+            transponder['downlink_high'] = transponder['downlink_low']
 
         # End previous NORAD entry, except for first iteration
-        if previous_norad_id != i['norad_cat_id'] and previous_norad_id != False: 
+        if previous_norad_id != transponder['norad_cat_id'] and previous_norad_id != False: 
             db.write("end\n")
 
         # Start new transponder entry
-        if i['norad_cat_id'] != previous_norad_id: 
-            db.write("%s\n" %i['uuid']) 
-            db.write("%s\n" %i['norad_cat_id'])
+        if transponder['norad_cat_id'] != previous_norad_id: 
+            db.write("%s\n" %transponder['uuid']) 
+            db.write("%s\n" %transponder['norad_cat_id'])
             db.write("No alat, alon\n")
             
-        previous_norad_id = i['norad_cat_id']
+        previous_norad_id = transponder['norad_cat_id']
         
         # Add transponder data
-        db.write("%s - Inverting = %r " %(i['description'], i['invert']))
-        if not i['baud']:
-            db.write("- Baud = %s " %i['baud'])
-        if not i['alive']:
+        db.write("%s - Inverting = %r " %(transponder['description'], transponder['invert']))
+        if not transponder['baud']:
+            db.write("- Baud = %s " %transponder['baud'])
+        if not transponder['alive']:
             db.write("Not currently alive\n")
         else:
             db.write("\n")
 
         # Convert frequency input to MHz
-        db.write("%f, %f\n" %(i['uplink_low']/1e6,i['uplink_high']/1e6))
-        db.write("%f, %f\n" %((i['downlink_low']/1e6),(i['downlink_high']/1e6)))
+        db.write("%f, %f\n" %(transponder['uplink_low']/1e6,transponder['uplink_high']/1e6))
+        db.write("%f, %f\n" %((transponder['downlink_low']/1e6),(transponder['downlink_high']/1e6)))
         db.write("No weekly schedule\n")
         db.write("No orbital schedule\n")
     
